46efd5663d001afcd39fdcb7fbd7301f3b6f6214
Nagendra Kathi <nagendra30244@gmail.com>
2025-09-01 18:40:18 +0530

    FIx import issues

--- client/src/pages/Profilepage.jsx
+++ client/src/pages/Profilepage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
-import { Camera, Mail, User } from "lucide-react";
+import { ArrowLeft, Camera, Mail, User } from "lucide-react";
 import { Link } from "react-router-dom";
 export default function Profilepage() {
   const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
